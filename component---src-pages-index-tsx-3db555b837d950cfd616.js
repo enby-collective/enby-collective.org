17d3bec4e7bd790e8c6bc393c87dd35f9472c311
Achim Schneider <achim@parity.io>
2023-01-15 18:58:16 +0100

    Updates

---
+++ component---src-pages-index-tsx-3db555b837d950cfd616.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_contentful_homepage_ts=self.webpackChunkgatsby_starter_contentful_homepage_ts||[]).push([[691],{7062:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var a=n(2060),r=n(6687),c=n(2448),s=n(3513),u=n(9871),o=["id","blocktype"];function l(e){var t=e.data.homepage;return r.createElement(c.Z,t,t.blocks.map((function(e){var t=e.id,n=e.blocktype,c=(0,a.Z)(e,o),l=s[n]||u.Z;return r.createElement(l,Object.assign({key:t},c))})))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-3db555b837d950cfd616.js.map
